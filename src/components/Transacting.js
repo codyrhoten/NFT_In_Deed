05a2375359a990a4324917dd34f194f69e640ed1
Cody Rhoten <rhoten_cody@yahoo.com>
2023-01-20 12:32:52 -0500

    change messages during transactions

--- src/components/Transacting.js
+++ src/components/Transacting.js
@@ -6,16 +6,16 @@ export default function Transacting(/* { isLoading } */) {
             <div className='flex flex-col pb-12'>
                 <h2 className='py-2 text-center'>Tokenization/Listing Process</h2>
                 <p className='p-4 my-3'>
-                    <b>Step 1:</b> Tokenize the house NFT and store it on the blockchain. Confirm the transaction on your wallet.
+                    <b>Step 1:</b> Tokenize the house NFT and store it on the blockchain. Confirm the transaction in your wallet.
                 </p>
                 <p className='p-4 my-3'>
                     <b>Step 2:</b> Wait for a few seconds for the transaction to be processed.
                 </p>
                 <p className='p-4 my-3'>
-                    <b>Step 3:</b> Allow NFT-in-Deed to sell the house. This comes with a 3% listing fee. Confirm the transaction on your wallet.
+                    <b>Step 3:</b> Now for the listing transaction: this comes with a 3% listing fee. Confirm the transaction in your wallet.
                 </p>
                 <p className='pt-1 p-4 my-3'>
-                    <b>Step 4:</b> Wait another few seconds for the transaction to be processed.
+                    <b>Step 4:</b> Wait another few seconds for the transaction to be processed. Then your house will be on the NFT-in-Deed Market for sale!
                 </p>
                 {/* {isLoading && (
                     <div className='flex justify-center'>

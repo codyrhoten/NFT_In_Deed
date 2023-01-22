4c7865e5ebd04f295a879bbc3c2182a4ac315bd4
Cody Rhoten <rhoten_cody@yahoo.com>
2023-01-22 13:34:39 -0500

    update transacting pg when reselling deed

--- src/pages/house-resale.js
+++ src/pages/house-resale.js
@@ -93,7 +93,33 @@ export default function HouseResale() {
     return (
         <>
             {isTransacting ? (
-                <Transacting /* isLoading={isLoading} */ />
+                <div className='flex justify-center mt-10'>
+                    <div className='flex flex-col pb-12'>
+                        <h2 className='py-2 text-center'>Tokenization/Listing Process</h2>
+                        <p className='p-4 my-3'>
+                            <b>Step 1:</b> Give approval to the Market to receive and later sell the deed. Confirm the transaction in your wallet.
+                        </p>
+                        <p className='p-4 my-3'>
+                            <b>Step 2:</b> Wait for a few seconds for the transaction to be processed.
+                        </p>
+                        <p className='p-4 my-3'>
+                            <b>Step 3:</b> Now for the listing transaction: this comes with a 3% listing fee. Confirm the transaction in your wallet.
+                        </p>
+                        <p className='pt-1 p-4 my-3'>
+                            <b>Step 4:</b> Wait another few seconds for the transaction to be processed. Then your house will be up on the NFT-in-Deed Market for sale again!
+                        </p>
+                        {/* {isLoading && (
+                        <div className='flex justify-center'>
+                            <Image
+                                src={'/loading-spinner.gif'}
+                                alt='loading spinner'
+                                width='175'
+                                height='175'
+                            />
+                        </div>
+                    )} */}
+                    </div>
+                </div>
             ) : (
                 <Container className='flex justify-center'>
                     <div className='w-1/2 flex flex-col pb-12'>

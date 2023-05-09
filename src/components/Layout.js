c22f8ede8627803a689bd8b3f5b00eb8968d8753
codyrhoten <rhoten_cody@yahoo.com>
2023-05-08 23:36:55 -0400

    Layout: keep inital modal showing until both MetaMask is installed & user is on Goerli network

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,24 +8,35 @@ import { Button, Container, Modal } from "react-bootstrap";
 
 export default function Layout({ children }) {
     const [walletAddress, setWallet] = useState('');
+    const [network, setNetwork] = useState('');
     const [status, setStatus] = useState('');
     const [metamaskInstalled, setMetamaskInstalled] = useState(false);
     const [show, setShow] = useState(true);
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    console.log(show);
 
-    useEffect(() => { 
-        getWallet() ;
-    }, 
-    [metamaskInstalled]);
+    useEffect(() => {
+        getWallet();
+    },
+        [metamaskInstalled, network]);
 
-    function walletListener() {
+    async function walletListener() {
         if (window.ethereum) {
             setMetamaskInstalled(true);
+            const chain = await getChain();
+            setNetwork(chain);
 
-            if (show) {
+            if (network === 'Goerli' && show) {
                 handleClose();
+            } else {
+                setStatus(
+                    <Modal show={show} centered>
+                        <Modal.Body>
+                            <p className='p-3'>
+                                In your MetaMask wallet, switch to the Goerli Test Network. You may have to turn on "show test networks" in your MetaMask settings.
+                            </p>
+                        </Modal.Body>
+                    </Modal>
+                );
             }
 
             ethereum.on('accountsChanged', async accounts => {
@@ -42,19 +53,18 @@ export default function Layout({ children }) {
                 <div style={{ marginTop: '150px' }}>
                     <Modal show={show} centered>
                         <Modal.Body>
-                            <p>
+                            <p className='p-3'>
                                 {" "}To use this marketplace, MetaMask must be installed in your browser. Click the button to see which browsers are supported.{" "}
                             </p>
                             <div className="text-center">
                                 <Button
-                                    className='mx-3 my-3 rounded px-3 py-2 shadow-lg'
+                                    className='rounded mb-3 px-3 py-2 shadow-lg'
                                     onClick={
                                         () => window.open('https://metamask.io/download.html', '_blank')
                                     }
                                 >
                                     Get MetaMask
                                 </Button>
-                                <p><i>Then, select the Goerli test network.</i></p>
                             </div>
                         </Modal.Body>
                     </Modal>
@@ -63,12 +73,23 @@ export default function Layout({ children }) {
         }
     }
 
+    async function getChain() {
+        if (window.ethereum) {
+            let chain = await ethereum.networkVersion;
+
+            if (chain === '5') {
+                chain = 'Goerli';
+            }
+
+            return chain;
+        }
+    }
+
     async function getWallet() {
         const { address, status } = getWalletConnected();
         setWallet(address);
         notify('Wallet', status);
         walletListener();
-        console.log(show);
     }
 
     async function walletButtonPressed() {
@@ -85,7 +106,7 @@ export default function Layout({ children }) {
                 walletAddress={walletAddress}
                 walletButtonPressed={walletButtonPressed}
             />
-            {!metamaskInstalled ? <WalletStatus status={status} /> : children}
+            {!metamaskInstalled || network !== 'Goerli' ? <WalletStatus status={status} /> : children}
             <ToastContainer />
         </Container>
     );
